aa32cf42f60e031e00db31590cf0b46aea810f83
Admin <leonnegro0101@gmail.com>
2023-06-06 01:32:38 -0400

    hoja de trucos

--- hellogit3.cs
+++ hellogit3.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("New version hellogit 3 v3!");
 Console.WriteLine("merge 2");
 
+Console.WriteLine("Hoja de trucos https://training.github.com/downloads/es_ES/github-git-cheat-sheet/");
