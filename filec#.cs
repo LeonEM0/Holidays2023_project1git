6b98120402af811cf160f1317daf4a798486706f
Admin <leonnegro0101@gmail.com>
2023-06-10 01:25:38 -0400

    primera modificacion

--- filec#.cs
+++ filec#.cs
@@ -1,12 +1,2 @@
-Console.WriteLine("Holamundodesdegit");
-Console.WriteLine("Texto escrito desde visual studio1");
-Console.WriteLine("second snapshot");
-Console.WriteLine("Holamundodesdegit");
-Console.WriteLine("Texto escrito desde visual studio1");
-Console.WriteLine("second snapshot");
-Console.WriteLine("Holamundodesdegit");
-Console.WriteLine("Texto escrito desde visual studio1");
-Console.WriteLine("second snapshot");
-Console.WriteLine("Holamundodesdegit");
-Console.WriteLine("Texto escrito desde visual studio1");
-Console.WriteLine("second snapshot");
+Console.WriteLine("NewHelloGithub");
+
